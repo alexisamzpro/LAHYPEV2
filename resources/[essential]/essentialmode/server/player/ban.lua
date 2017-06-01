6ce8924b3e7335df28c0a8e5e50c1452dd3e0ebb
alexisamzpro <alexisamzpro@outlook.Fr>
2017-06-01 22:49:04 +0200

    Add files via upload

---
+++ resources/[essential]/essentialmode/server/player/ban.lua
@@ -0,0 +1,17 @@
+﻿local ban_list = {
+	[""] = true
+}
+
+RegisterServerEvent('playerConnecting')
+AddEventHandler('playerConnecting', function(name, setCallback)
+	local identifiers = GetPlayerIdentifiers(source)
+	for i = 1, #identifiers do
+		local ip = string.sub(identifiers[i], 4)
+		if ban_list[ip] ~= nil then 
+			print('Player that was banned tried to join: ' .. ip)
+			setCallback("Vous etes ban du serveur.")
+			CancelEvent()
+			return
+		end
+	end
+end)
